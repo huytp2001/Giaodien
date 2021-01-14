acff030a42b9267d302967211cf7743ab25d9ff6
Huy <dqhuy.tp2001@gmail.com>
2021-01-14 21:37:12 +0700

    ver1

--- script1.js
+++ script1.js
@@ -11,18 +11,22 @@ function mobiphone() {
     document.getElementById("cnm_img").src = "/img/Image 31.png";
     sessionStorage.setItem("path_img", "/img/Image 31.png")
 }
+
 function viettel() {
     document.getElementById("cnm_img").src = "/img/Image 32.png";
     sessionStorage.setItem("path_img", "/img/Image 32.png")
 }
+
 function vinaphone() {
     document.getElementById("cnm_img").src = "/img/Image 33.png";
     sessionStorage.setItem("path_img", "/img/Image 33.png")
 }
+
 function vietnammobile() {
     document.getElementById("cnm_img").src = "/img/Image 34.png";
     sessionStorage.setItem("path_img", "/img/Image 34.png")
 }
+
 function gmobile() {
     document.getElementById("cnm_img").src = "/img/gmobile.png";
     sessionStorage.setItem("path_img", "/img/gmobile.png")
@@ -36,12 +40,15 @@ function get_data() {
 function a1() {
     sessionStorage.setItem("currency", "10,000");
 }
+
 function a2() {
     sessionStorage.setItem("currency", "20,000");
 }
+
 function a3() {
     sessionStorage.setItem("currency", "30,000");
 }
+
 function a4() {
     sessionStorage.setItem("currency", "50,000");
 }
@@ -49,12 +56,15 @@ function a4() {
 function b1() {
     sessionStorage.setItem("currency", "100,000");
 }
+
 function b2() {
     sessionStorage.setItem("currency", "200,000");
 }
+
 function b3() {
     sessionStorage.setItem("currency", "300,000");
 }
+
 function b4() {
     sessionStorage.setItem("currency", "500,000");
 }
